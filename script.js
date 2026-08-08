f4e7833153c0a1803992299675b07d304eda0a09
Baranikarthick M <baranikarthick21@gmail.com>
2026-08-09 01:42:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -263,6 +263,7 @@
       { name:'Cloud Computing', cat:'Cloud Computing', logo:'assets/logos/cloud-logo.webp', pct:74 },
       { name:'ChatGPT', cat:'Generative AI', logo:'assets/logos/ChatGPT-Logo.png', pct:85 },
       { name:'Claude AI', cat:'Generative AI', logo:'assets/logos/Claude-AI-logo.png', pct:85 }
+      { name:'mongodb', cat:'Backend Devlopment', logo:'assets/logos/mongodb-logo.png', pct:90 }
     ];
 
     const durations = ['4.8s','5.2s','5.8s','6.2s','6.8s','7.2s'];
